7a696136d48127a12862ddb66a95eb86dc57fa95
Isaac Lew <lewyzisaac@gmail.com>
2020-08-15 18:29:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def solution(n):
+    #Your code here
+    #this is basically a code to find out how many different ways there are to add up to a particular integer using unique integers only
+    #this is partitions of number theory and looking for unique partition
+    #odd partition = distinct partition
+    
