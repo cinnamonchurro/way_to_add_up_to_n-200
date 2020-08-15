1af5eac2c8256edfd96e87771b577b093ceecf05
Isaac Lew <lewyzisaac@gmail.com>
2020-08-15 18:48:56 +0000

    triangular numbers governs maximum distinct summands

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ def solution(n):
     #this is basically a code to find out how many different ways there are to add up to a particular integer using unique integers only
     #this is partitions of number theory and looking for unique partition
     #odd partition = distinct partition
-    #maximum number of distinct numbers used is governed by triangular numbers 
     matrix=[[0]*(n+1) for i in range(n+1)]
     print(matrix)
 
